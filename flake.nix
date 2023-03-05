f5bc7d996d31008fbbd309f1c8537f4e99b4e62f
Astrid Yu <astrid@astrid.tech>
2023-03-04 21:24:17 -0800

    Downgrade to stable rust instead of unstable

--- flake.nix
+++ flake.nix
@@ -1,23 +1,22 @@
 {
   inputs = {
     naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, rust-overlay }:
-    utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, naersk, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ rust-overlay.overlays.default ];
         };
 
-        rust-toolchain = (pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-          toolchain.default.override {
-            targets = [ "x86_64-unknown-linux-musl" ];
-          }));
+        rust-toolchain = (pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "x86_64-unknown-linux-musl" ];
+        });
         rust-toolchain-dev = rust-toolchain.override {
           extensions = [ "rust-src" "rust-analyzer" ];
         };
