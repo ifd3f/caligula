a40e4930aa8c4661d142abe7b76be291ddf4cd28
Astrid Yu <astrid@astrid.tech>
2023-03-05 16:07:05 -0800

    Set F_NOCACHE on the blockdevs on macos

--- src/burn/xplat.rs
+++ src/burn/xplat.rs
@@ -3,19 +3,34 @@ use std::{
     path::Path,
 };
 
-#[cfg(any(target_os = "macos", target_os = "linux"))]
+#[cfg(target_os = "linux")]
 pub fn open_blockdev(path: impl AsRef<Path>) -> std::io::Result<File> {
     use nix::fcntl::OFlag;
     use std::os::unix::fs::OpenOptionsExt;
 
-    #[cfg(target_os = "macos")]
-    let oflag = OFlag::O_SYNC;
-
-    #[cfg(target_os = "linux")]
-    let oflag = OFlag::O_DIRECT | OFlag::O_SYNC;
-
     OpenOptions::new()
         .write(true)
-        .custom_flags(oflag.bits())
+        .custom_flags(OFlag::O_DIRECT | OFlag::O_SYNC)
         .open(path)
+}
+
+#[cfg(target_os = "macos")]
+pub fn open_blockdev(path: impl AsRef<Path>) -> std::io::Result<File> {
+    // For more info, see:
+    // https://stackoverflow.com/questions/2299402/how-does-one-do-raw-io-on-mac-os-x-ie-equivalent-to-linuxs-o-direct-flag
+
+    use libc::{O_SYNC, F_NOCACHE, fcntl};
+    use std::os::{unix::fs::OpenOptionsExt, fd::AsRawFd};
+
+    let file = OpenOptions::new()
+        .write(true)
+        .custom_flags(O_SYNC)
+        .open(path)?;
+
+    #[cfg(target_os = "macos")]
+    unsafe {
+        fcntl(file.as_raw_fd().into(), F_NOCACHE);
+    }
+
+    Ok(file)
 }
