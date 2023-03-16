e94d6ec2471e676e3fb9c9c005c706ee6ed696cc
Astrid Yu <astrid@astrid.tech>
2023-03-15 21:38:52 -0700

    Write checks for headless mode

---
+++ checks/headless/default.nix
@@ -0,0 +1,17 @@
+{ lib, caligula, runCommand }:
+runCommand "caligula-headless-test" {
+  buildInputs = [ caligula ];
+  isoInnerHash = "3972dc9744f6499f0f9b2dbf76696f2ae7ad8af9b23dde66d6af86c9dfb36986";
+  meta.timeout = 10;
+} ''
+  caligula burn ${./input.iso.gz} \
+    --force \
+    -o /build/out.iso \
+    --hash $isoInnerHash \
+    --hash-of raw \
+    --compression auto 
+
+  diff ${./expected.iso} /build/out.iso
+
+  echo 1 > $out
+''
