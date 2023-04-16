331623065c113b99f864ec6debf21dd90f7fe84b
Astrid Yu <astrid@astrid.tech>
2023-04-15 22:11:38 -0700

    Write utility for generic root escalation

---
+++ src/escalation/darwin.rs
@@ -0,0 +1,27 @@
+use std::process::Command;
+
+use tokio::process::Command as AsyncCommand;
+
+use super::unix::EscalationMethod;
+
+pub async fn wrap_osascript_escalation(raw: Command) -> anyhow::Result<tokio::process::Child> {
+    for _ in 0..3 {
+        // User-friendly thing that lets you use touch ID if you wanted.
+        // https://apple.stackexchange.com/questions/23494/what-option-should-i-give-the-sudo-command-to-have-the-password-asked-through-a
+        // We loop because your finger might not be recognized sometimes.
+
+        let result = AsyncCommand::new("osascript")
+            .arg("-e")
+            .arg("do shell script \"mkdir -p /var/db/sudo/$USER; touch /var/db/sudo/$USER\" with administrator privileges")
+            .spawn()?
+            .wait()
+            .await?;
+
+        if result.success() {
+            break;
+        }
+    }
+
+    let cmd: Command = EscalationMethod::Sudo.wrap_command(raw).into();
+    Ok(AsyncCommand::from(cmd).spawn()?)
+}
---
+++ src/escalation/mod.rs
@@ -0,0 +1,28 @@
+mod darwin;
+mod unix;
+
+use std::process::Command;
+use tokio::process::Command as AsyncCommand;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Could not become root! Searched for sudo, doas, su")]
+    UnixNotDetected,
+    #[error("User failed to confirm")]
+    MacOSDenial,
+}
+
+#[cfg(target_os = "linux")]
+pub async fn run_escalate(cmd: Command) -> anyhow::Result<tokio::process::Child> {
+    use self::unix::EscalationMethod;
+
+    let mut cmd: AsyncCommand = EscalationMethod::detect()?.wrap_command(cmd).into();
+    Ok(cmd.spawn()?)
+}
+
+#[cfg(target_os = "macos")]
+pub fn run_escalate(cmd: Command) -> anyhow::Result<tokio::process::Child> {
+    use self::darwin::wrap_osascript_escalation;
+
+    wrap_osascript_escalation(cmd)
+}
