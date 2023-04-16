e81b7be22e182a0cce986a3a6d292a8a5f7651b0
Astrid Yu <astrid@astrid.tech>
2023-04-15 23:55:11 -0700

    Enable kvm feature on CI

--- checks/default.nix
+++ checks/default.nix
@@ -6,10 +6,14 @@ let
     overlays = [ self.overlays.default ];
   };
 in {
+  headless = pkgs.callPackage ./headless { };
+  smoke-test-simple = pkgs.callPackage ./smoke-test-simple { };
+} //
+
+(if system == "x86_64-linux" then {
   autoescalate-doas =
     pkgs.callPackage ./autoescalate { escalationTool = "doas"; };
   autoescalate-sudo =
     pkgs.callPackage ./autoescalate { escalationTool = "sudo"; };
-  headless = pkgs.callPackage ./headless { };
-  smoke-test-simple = pkgs.callPackage ./smoke-test-simple { };
-}
+} else
+  { })
