08293ed7f17bb191af1a22a0d38308ae669a0f4b
Astrid Yu <astrid@astrid.tech>
2023-08-20 19:47:48 -0700

    Improve root escalation system

--- src/escalation/unix.rs
+++ src/escalation/unix.rs
@@ -1,5 +1,7 @@
+use std::borrow::Cow;
+
 use itertools::Itertools;
-use std::{ffi::OsString, process::Command};
+use shell_words::{join, quote};
 use which::which;
 
 use super::Error;
@@ -12,6 +14,14 @@ pub enum EscalationMethod {
     Doas,
     #[display(fmt = "su")]
     Su,
+}
+
+/// Command components, backed by copy-on-write storage.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Command<'a> {
+    pub envs: Vec<(Cow<'a, str>, Cow<'a, str>)>,
+    pub proc: Cow<'a, str>,
+    pub args: Vec<Cow<'a, str>>,
 }
 
 impl EscalationMethod {
@@ -38,86 +48,144 @@ impl EscalationMethod {
         }
     }
 
-    pub fn wrap_command<'a>(&self, cmd: Command) -> Command {
-        // Yes this is jank. However, it's good enough for our purposes.
-        let envs: String = cmd
-            .get_envs()
-            .map(|(k, v)| {
-                format!(
-                    "{}={}",
-                    k.to_string_lossy(),
-                    v.unwrap_or(&OsString::from("")).to_string_lossy()
-                )
-            })
-            .join(" ");
-
-        let raw = format!("{envs} {cmd:?}");
+    pub fn wrap_command<'a>(&self, cmd: &Command) -> Command {
+        let raw = cmd.to_string();
 
         match self {
-            Self::Sudo => {
-                let mut cmd = Command::new("sudo");
-                cmd.args(["sh", "-c", &raw]);
-                cmd
-            }
-            Self::Doas => {
-                let mut cmd = Command::new("doas");
-                cmd.args(["sh", "-c", &raw]);
-                cmd
-            }
-            Self::Su => {
-                let mut cmd = Command::new("su");
-                cmd.args(["root", "-c", "sh", "-c", &raw]);
-                cmd
-            }
+            Self::Sudo => Command {
+                envs: vec![],
+                proc: "sudo".into(),
+                args: vec!["sh".into(), "-c".into(), raw.into()],
+            },
+            Self::Doas => Command {
+                envs: vec![],
+                proc: "doas".into(),
+                args: vec!["sh".into(), "-c".into(), raw.into()],
+            },
+            Self::Su => Command {
+                envs: vec![],
+                proc: "su".into(),
+                args: vec![
+                    "root".into(),
+                    "-c".into(),
+                    "sh".into(),
+                    "-c".into(),
+                    raw.into(),
+                ],
+            },
         }
+    }
+}
+
+impl ToString for Command<'_> {
+    fn to_string(&self) -> String {
+        let args = join([&self.proc].into_iter().chain(self.args.iter()));
+
+        if self.envs.is_empty() {
+            args
+        } else {
+            let envs: String = (self.envs.iter())
+                .map(|(k, v)| format!("{}={}", quote(k), quote(v)))
+                .join(" ");
+
+            format!("{envs} {args}")
+        }
+    }
+}
+
+impl From<Command<'_>> for std::process::Command {
+    fn from(value: Command<'_>) -> Self {
+        let mut c = std::process::Command::new(value.proc.as_ref());
+        c.args(value.args.iter().map(|a| a.as_ref()));
+        c.envs(value.envs.iter().map(|(k, v)| (k.as_ref(), v.as_ref())));
+        c
+    }
+}
+
+impl From<Command<'_>> for tokio::process::Command {
+    fn from(value: Command<'_>) -> Self {
+        std::process::Command::from(value).into()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::process::Command;
+    use super::*;
 
-    use super::EscalationMethod;
+    #[test]
+    fn test_to_string_no_env() {
+        let command = Command {
+            envs: vec![],
+            proc: "foo bar".into(),
+            args: vec![
+                "mrrrrp\\x12 mrp nya nya!".into(),
+                "yip yip".into(),
+                "yip".into(),
+            ],
+        };
 
-    fn get_test_command() -> Command {
-        let mut cmd = Command::new("some/proc");
-        cmd.arg("two")
-            .arg("--three")
-            .arg("\"four\"")
-            .env("asdf", "foo");
-        cmd
+        let result = command.to_string();
+
+        assert_eq!(result, "'foo bar' 'mrrrrp\\x12 mrp nya nya!' 'yip yip' yip")
+    }
+
+    #[test]
+    fn test_to_string_with_env() {
+        let command = Command {
+            envs: vec![("uwu".into(), "nyaaa aaa!".into())],
+            proc: "foo bar".into(),
+            args: vec![
+                "mrrrrp\\x12 mrp nya nya!".into(),
+                "yip yip".into(),
+                "yip".into(),
+            ],
+        };
+
+        let result = command.to_string();
+
+        assert_eq!(
+            result,
+            "uwu='nyaaa aaa!' 'foo bar' 'mrrrrp\\x12 mrp nya nya!' 'yip yip' yip"
+        )
+    }
+
+    fn get_test_command() -> Command<'static> {
+        Command {
+            envs: vec![("asdf".into(), "foo".into())],
+            proc: "some/proc".into(),
+            args: vec!["two".into(), "--three".into(), "\"four\"".into()],
+        }
     }
 
     #[test]
     fn test_sudo() {
-        let result = EscalationMethod::Sudo.wrap_command(get_test_command());
+        let result = EscalationMethod::Sudo.wrap_command(&get_test_command());
 
-        let printed = format!("{result:?}");
         assert_eq!(
-            printed,
-            r#""sudo" "sh" "-c" "asdf=foo \"some/proc\" \"two\" \"--three\" \"\\\"four\\\"\"""#
+            result.to_string(),
+            "sudo sh -c 'asdf=foo some/proc two --three '\\''\"four\"'\\'''"
         )
     }
 
     #[test]
     fn test_doas() {
-        let result = EscalationMethod::Doas.wrap_command(get_test_command());
+        let result = EscalationMethod::Doas.wrap_command(&get_test_command());
 
         let printed = format!("{result:?}");
         assert_eq!(
-            printed,
-            r#""doas" "sh" "-c" "asdf=foo \"some/proc\" \"two\" \"--three\" \"\\\"four\\\"\"""#
+            result.to_string(),
+            "doas sh -c 'asdf=foo some/proc two --three '\\''\"four\"'\\'''"
         )
     }
 
     #[test]
     fn test_su() {
-        let result = EscalationMethod::Su.wrap_command(get_test_command());
+        let result = EscalationMethod::Su.wrap_command(&get_test_command());
 
         let printed = format!("{result:?}");
         assert_eq!(
-            printed,
-            r#""su" "root" "-c" "sh" "-c" "asdf=foo \"some/proc\" \"two\" \"--three\" \"\\\"four\\\"\"""#
+            result.to_string(),
+            "su root -c sh -c 'asdf=foo some/proc two --three '\\''\"four\"'\\'''"
         )
     }
 }
