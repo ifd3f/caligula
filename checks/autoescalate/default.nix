6936e5136e06206f9e79613572c034917f92afeb
Astrid Yu <astrid@astrid.tech>
2024-05-13 17:03:16 -0700

    Make the autoescalate checks print logs

--- checks/autoescalate/default.nix
+++ checks/autoescalate/default.nix
@@ -35,12 +35,15 @@ nixosTest {
     };
 
   testScript = ''
-    # Set up loop devices
-    machine.succeed('dd if=/dev/zero of=/tmp/blockfile bs=1M count=1')
-    machine.succeed('dd if=/dev/urandom of=/tmp/input.iso bs=100K count=1')
-    machine.succeed('losetup /dev/loop0 /tmp/blockfile')
+    try:
+        # Set up loop devices
+        machine.succeed('dd if=/dev/zero of=/tmp/blockfile bs=1M count=1')
+        machine.succeed('dd if=/dev/urandom of=/tmp/input.iso bs=100K count=1')
+        machine.succeed('losetup /dev/loop0 /tmp/blockfile')
 
-    with subtest("should succeed when run as non-root wheel user"):
-        machine.succeed('timeout 10 su admin -c "caligula burn /tmp/input.iso --force -o /dev/loop0 --hash skip --compression auto --root always --interactive never"')
+        with subtest("should succeed when run as non-root wheel user"):
+            machine.succeed('timeout 10 su admin -c "caligula burn /tmp/input.iso --force -o /dev/loop0 --hash skip --compression auto --root always --interactive never"')
+    finally: 
+        print(machine.execute('bash -c "cat /tmp/caligula-*/log/*"', check_output=True)[1])
   '';
 }
