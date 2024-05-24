664fd4fc069b6af4f9f9cb3745826aba7a875411
Astrid Yu <astrid@astrid.tech>
2024-05-23 22:04:17 -0700

    Add a matrix of blocky burny tests

--- checks/headless/default.nix
+++ checks/headless/default.nix
@@ -1,7 +1,8 @@
 { lib, caligula, runCommand }:
 runCommand "caligula-headless-test" {
   buildInputs = [ caligula ];
-  isoInnerHash = "3972dc9744f6499f0f9b2dbf76696f2ae7ad8af9b23dde66d6af86c9dfb36986";
+  isoInnerHash =
+    "3972dc9744f6499f0f9b2dbf76696f2ae7ad8af9b23dde66d6af86c9dfb36986";
   meta.timeout = 10;
 } ''
   caligula burn ${./input.iso.gz} \
