ee6d4934ab0376fa2b6427bb7c0624e169acc413
Astrid Yu <astrid@astrid.tech>
2026-04-26 21:58:35 -0700

    state transition handlers

--- stdiomux/src/adm.rs
+++ stdiomux/src/adm.rs
@@ -7,7 +7,7 @@ use bytes::Bytes;
 use futures::{Sink, Stream};
 use tokio::sync::{mpsc, oneshot};
 
-use crate::mux::{Frame, ChannelId as ChannelId};
+use crate::mux::{ChannelId, Frame};
 
 pub struct AdmissionController<Rx, Tx>
 where
@@ -104,7 +104,7 @@ where
                 state: ChannelState::Closed,
             })
             .state
-            .transition_send_syn(our_rx_buffer as u64)?;
+            .try_transition_send_syn(our_rx_buffer as u64)?;
         self.high_pri_tx_queue_appender
             .send((channel_id, frame))
             .unwrap();
@@ -131,14 +131,15 @@ enum ChannelState {
         their_rx_buffer: u64,
     },
     Established(ChannelEstablished),
-    SentClose,
-    RecvdClose,
+    SentClose {
+        tx_on_closed: oneshot::Sender<()>,
+    },
 }
 
 impl ChannelState {
     /// Ensures that it's allowable to send a SYN. If it is allowable, returns the SYN to send,
     /// along with a oneshot::Receiver for waiting on channel establishment.
-    fn transition_send_syn(
+    fn try_transition_send_syn(
         &mut self,
         our_rx_buffer: u64,
     ) -> Result<(Frame, oneshot::Receiver<ChannelEstablishmentInfo>), ChannelInUse> {
@@ -165,8 +166,119 @@ impl ChannelState {
             }
             ChannelState::SentOpen { .. }
             | ChannelState::Established(_)
-            | ChannelState::SentClose
-            | ChannelState::RecvdClose => Err(ChannelInUse),
+            | ChannelState::SentClose { .. } => Err(ChannelInUse),
+        }
+    }
+
+    /// Ensures that it's allowable to send a FIN. If it is allowable, returns the FIN to send,
+    /// along with a oneshot::Receiver for waiting on channel closing.
+    fn try_transition_send_fin(&mut self) -> Result<(Frame, oneshot::Receiver<()>), AlreadyClosed> {
+        match self {
+            ChannelState::Closed | ChannelState::SentClose { .. } => Err(AlreadyClosed),
+            ChannelState::Established(_)
+            | ChannelState::RecvdOpen { .. }
+            | ChannelState::SentOpen { .. } => {
+                let (tx_on_closed, rx_on_closed) = oneshot::channel();
+                *self = ChannelState::SentClose { tx_on_closed };
+                Ok((Frame::Fin, rx_on_closed))
+            }
+        }
+    }
+
+    /// Handle receiving a frame. Returns the frame to send in response, if any.
+    fn transition_recv(&mut self, frame: Frame) -> Option<Frame> {
+        match frame {
+            Frame::Reset => {
+                self.transition_recv_rst();
+                None
+            }
+            Frame::Data(bytes) => self.transition_recv_data(bytes),
+            Frame::Adm(permits) => self.transition_recv_adm(permits),
+            Frame::Syn(buf) => self.transition_recv_syn(buf),
+            Frame::Fin => self.transition_recv_fin(),
+        }
+    }
+
+    /// Handle receiving a DAT. Returns the frame to send in response, if any.
+    fn transition_recv_data(&mut self, body: Bytes) -> Option<Frame> {
+        match self {
+            ChannelState::Established(channel_established) => {
+                channel_established
+                    .rx
+                    .tx_to_consumer
+                    .try_send(body)
+                    .expect("Failed");
+                None // Don't send ADM yet -- allow the frames to bunch up before ADM
+            }
+            ChannelState::SentClose { .. } => None,
+            ChannelState::Closed
+            | ChannelState::SentOpen { .. }
+            | ChannelState::RecvdOpen { .. } => {
+                tracing::warn!("Unexpectedly received ADM!");
+                *self = ChannelState::Closed;
+                Some(Frame::Reset)
+            }
+        }
+    }
+
+    /// Handle receiving a SYN. Returns the frame to send in response, if any.
+    fn transition_recv_syn(&mut self, their_rx_buffer: u64) -> Option<Frame> {
+        match self {
+            ChannelState::Closed | ChannelState::RecvdOpen { .. } => {
+                *self = ChannelState::RecvdOpen { their_rx_buffer };
+                None
+            }
+            ChannelState::SentOpen { .. }
+            | ChannelState::Established(_)
+            | ChannelState::SentClose { .. } => {
+                tracing::warn!("Unexpectedly received SYN!");
+                *self = ChannelState::Closed;
+                Some(Frame::Reset)
+            }
+        }
+    }
+
+    /// Handle receiving a RST (AKA it just closes)
+    fn transition_recv_rst(&mut self) {
+        *self = ChannelState::Closed;
+    }
+
+    /// Handle receiving a FIN
+    fn transition_recv_fin(&mut self) -> Option<Frame> {
+        match self {
+            ChannelState::Established(_)
+            | ChannelState::SentOpen { .. }
+            | ChannelState::RecvdOpen { .. } => {
+                *self = ChannelState::Closed;
+                Some(Frame::Fin)
+            }
+            ChannelState::SentClose { .. } => {
+                *self = ChannelState::Closed;
+                None
+            }
+            ChannelState::Closed => {
+                tracing::warn!("Unexpectedly received FIN!");
+                *self = ChannelState::Closed;
+                Some(Frame::Reset)
+            }
+        }
+    }
+
+    /// Handle receiving an ADM
+    fn transition_recv_adm(&mut self, permits: u64) -> Option<Frame> {
+        match self {
+            ChannelState::Established(channel_established) => {
+                channel_established.tx.outstanding_permits += permits;
+                None
+            }
+            ChannelState::SentClose { .. } => None,
+            ChannelState::Closed
+            | ChannelState::SentOpen { .. }
+            | ChannelState::RecvdOpen { .. } => {
+                tracing::warn!("Unexpectedly received ADM!");
+                *self = ChannelState::Closed;
+                Some(Frame::Reset)
+            }
         }
     }
 }
@@ -176,7 +288,7 @@ impl ChannelState {
 pub struct ChannelInUse;
 
 #[derive(Debug, thiserror::Error)]
-#[error("oops already closed")]
+#[error("Channel already closed")]
 struct AlreadyClosed;
 
 pub struct ChannelEstablishmentInfo {
@@ -194,7 +306,9 @@ impl ChannelEstablished {
         let (tx_to_channel_map, rx_from_consumer) = mpsc::channel(tx_buffer);
         let (tx_to_consumer, rx_from_channel_map) = mpsc::channel(rx_buffer);
         let sender = Sender { tx_to_channel_map };
-        let receiver = Receiver { rx_from_channel_map };
+        let receiver = Receiver {
+            rx_from_channel_map,
+        };
         (
             ChannelEstablished {
                 tx: ChannelTxState {
