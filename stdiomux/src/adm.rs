0b849c81dd27d61fc0a735a15c5bee904b455ff4
Astrid Yu <astrid@astrid.tech>
2026-04-27 00:41:52 -0700

    okay let's just make it actory

--- stdiomux/src/adm.rs
+++ stdiomux/src/adm.rs
@@ -1,21 +1,25 @@
-use std::{
-    collections::{HashMap, VecDeque, hash_map::Entry},
-    task::{Context, Poll},
-};
+use std::marker::PhantomData;
 
 use bytes::Bytes;
-use futures::{Sink, Stream};
+use dashmap::DashMap;
+use futures::{Sink, SinkExt, StreamExt, TryStream};
+use std::fmt::Debug;
 use std::sync::Arc;
-use tokio::sync::{mpsc, oneshot};
+use tokio::sync::{mpsc, oneshot, watch};
 
 use crate::mux::{ChannelId, Frame};
 
 pub struct AdmissionController<Rx, Tx>
 where
-    Rx: Stream<Item = (ChannelId, Frame)>,
-    Tx: Sink<(ChannelId, Frame)>,
+    Rx: TryStream<Item = (ChannelId, Frame)> + Unpin,
+    Tx: Sink<(ChannelId, Frame)> + Unpin,
 {
-    inner: Arc<std::sync::Mutex<AdmissionControllerInner<Rx, Tx>>>,
+    inner: Arc<AdmissionControllerInner>,
+    /// Queue of frames to transmit.
+    tx_queue_appender: mpsc::Sender<(ChannelId, Frame)>,
+    /// Queue of frames to transmit.
+    high_pri_tx_queue_appender: mpsc::UnboundedSender<(ChannelId, Frame)>,
+    _phantom: PhantomData<(Rx, Tx)>,
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -28,126 +32,170 @@ pub enum OpenChannelError {
 
 impl<Rx, Tx> AdmissionController<Rx, Tx>
 where
-    Rx: Stream<Item = (ChannelId, Frame)>,
-    Tx: Sink<(ChannelId, Frame)>,
+    Rx: TryStream<Item = (ChannelId, Frame)> + Unpin + Send + 'static,
+    Tx: Sink<(ChannelId, Frame)> + Unpin + Send + 'static,
+    <Tx as futures::Sink<(ChannelId, Frame)>>::Error: Debug,
 {
-    pub fn new(rx: Rx, tx: Tx) -> Self {
-        let (tx_queue_appender, tx_queue) = mpsc::channel(128);
-        let (high_pri_tx_queue_appender, high_pri_tx_queue) = mpsc::unbounded_channel();
-        let inner = Arc::new(std::sync::Mutex::new(AdmissionControllerInner {
-            rx,
-            tx,
-            channels: HashMap::new(),
-            tx_queue,
-            tx_queue_appender,
-            high_pri_tx_queue,
-            high_pri_tx_queue_appender,
-        }));
+    pub fn new(mut rx: Rx, mut tx: Tx) -> Self {
+        let (tx_queue_appender, mut tx_queue) = mpsc::channel(128);
+        let (high_pri_tx_queue_appender, mut high_pri_tx_queue) = mpsc::unbounded_channel();
+        let inner_value = Arc::new(AdmissionControllerInner {
+            channels: DashMap::new(),
+        });
 
-        Self { inner }
+        let _rx_actor = tokio::spawn({
+            let inner = inner_value.clone();
+            let high_pri_tx_queue_appender = high_pri_tx_queue_appender.clone();
+            async move {
+                while let Some((channel_id, frame)) = rx.next().await {
+                    // try to forward the message to the actor
+                    let Some(entry) = inner.channels.get(&channel_id) else {
+                        high_pri_tx_queue_appender
+                            .send((channel_id, Frame::Reset))
+                            .unwrap();
+                        return;
+                    };
+                    let send = entry.handle_frame(frame);
+
+                    if let Some(send) = send {
+                        let Ok(()) = high_pri_tx_queue_appender.send((channel_id, send)) else {
+                            todo!()
+                        };
+                    }
+                }
+            }
+        });
+
+        let _tx_actor = tokio::spawn(async move {
+            loop {
+                tokio::select! {
+                    biased;
+                    v = high_pri_tx_queue.recv() => {
+                        let Some(v) = v else { return; };
+                        tx.send(v).await.unwrap()
+                    }
+                    v = tx_queue.recv() => {
+                        let Some(v) = v else { return; };
+                        tx.send(v).await.unwrap()
+                    }
+                }
+            }
+        });
+
+        Self {
+            inner: inner_value,
+            tx_queue_appender,
+            high_pri_tx_queue_appender,
+            _phantom: PhantomData,
+        }
     }
 
     pub async fn open_channel(
         &self,
         channel_id: ChannelId,
         initial_rx_buffer: usize,
-    ) -> Result<ChannelEstablishmentInfo, OpenChannelError> {
-        let rx = self
-            .inner
-            .lock()
-            .unwrap()
-            .open_channel(channel_id, initial_rx_buffer)?;
-        Ok(rx.await?)
+    ) -> Result<ChannelIo, OpenChannelError> {
+        let (frame, rx) = self.inner.open_channel(channel_id, initial_rx_buffer)?;
+        let Ok(()) = self.high_pri_tx_queue_appender.send((channel_id, frame)) else {
+            todo!()
+        };
+
+        let tx_queue_appender = self.tx_queue_appender.clone();
+        let (mut actor_wires, io) = rx.await?;
+
+        let _channel_tx_actor = tokio::spawn(async move {
+            let mut next_seqno = 0u64;
+
+            loop {
+                // grab send from user
+                let next_tx = actor_wires.rx_from_user.recv();
+
+                // wait until max_seqno allows for this send to happen
+                let adm_enough = async {
+                    actor_wires
+                        .rx_max_seqno
+                        .wait_for(|max_seqno| *max_seqno >= next_seqno)
+                        .await?;
+                    Ok::<_, watch::error::RecvError>(())
+                };
+
+                let x = tokio::join!(next_tx, adm_enough);
+                match x {
+                    // if either of these is disconnected, then we're done
+                    (None, _) | (_, Err(_)) => break,
+
+                    // send the packet over
+                    (Some(bs), Ok(_)) => {
+                        let Ok(()) = tx_queue_appender.send((channel_id, Frame::Data(bs))).await
+                        else {
+                            break;
+                        };
+                    }
+                }
+
+                // increment seqno
+                next_seqno += 1;
+            }
+        });
+
+        Ok(io)
     }
 }
 
 pub struct Sender {
-    tx_to_channel_map: mpsc::Sender<Bytes>,
+    tx_to_actor: mpsc::Sender<Bytes>,
 }
 
 pub struct Receiver {
     rx_from_channel_map: mpsc::Receiver<Bytes>,
 }
 
-struct AdmissionControllerInner<Rx, Tx>
-where
-    Rx: Stream<Item = (ChannelId, Frame)>,
-    Tx: Sink<(ChannelId, Frame)>,
-{
-    rx: Rx,
-    tx: Tx,
+struct AdmissionControllerInner {
     /// Active, non-closed channels.
-    channels: HashMap<ChannelId, ChannelMapEntry>,
-    /// Queue of frames to transmit.
-    tx_queue: mpsc::Receiver<(ChannelId, Frame)>,
-    /// Queue of frames to transmit.
-    tx_queue_appender: mpsc::Sender<(ChannelId, Frame)>,
-    /// Queue of frames to transmit.
-    high_pri_tx_queue: VecDeque<>
+    channels: DashMap<ChannelId, ChannelMapEntry>,
 }
 
-impl<Rx, Tx> AdmissionControllerInner<Rx, Tx>
-where
-    Rx: Stream<Item = (ChannelId, Frame)>,
-    Tx: Sink<(ChannelId, Frame)>,
-{
-    pub fn open_channel(
-        &mut self,
+impl AdmissionControllerInner {
+    fn open_channel(
+        &self,
         channel_id: ChannelId,
         our_rx_buffer: usize,
-    ) -> Result<oneshot::Receiver<ChannelEstablishmentInfo>, ChannelInUse> {
-        let (frame, out) = self
-            .channels
+    ) -> Result<(Frame, oneshot::Receiver<(ChannelTxActorWires, ChannelIo)>), ChannelInUse> {
+        self.channels
             .entry(channel_id)
             .or_insert_with(|| ChannelMapEntry {
-                state: ChannelState::Closed,
+                state: std::sync::Mutex::new(ChannelState::Closed),
             })
             .state
-            .try_transition_send_syn(our_rx_buffer as u64)?;
-        self.high_pri_tx_queue_appender
-            .send((channel_id, frame))
-            .unwrap();
-        Ok(out)
-    }
-
-    pub fn poll(&mut self, cx: &mut Context<'_>) {
-        let mut remove_ids = vec![];
-        for (id, entry ) in &mut self.channels {
-            match entry.state.poll_next_frame(cx) {
-                Poll::Ready(Some(v)) => match v {
-                    Frame::Reset => todo!(),
-                    Frame::Data(bytes) => todo!(),
-                    Frame::Adm(_) => self.high_pri_tx_queue_appender.send(v).unwrap(),
-                    Frame::Syn(_) => todo!(),
-                    Frame::Fin => todo!(),
-                }
-                Poll::Ready(None) => {remove_ids.push(*id);},
-                Poll::Pending => (),
-            }
-        }
-
+            .lock()
+            .unwrap()
+            .try_transition_send_syn(our_rx_buffer as u64)
     }
 }
 
 struct ChannelMapEntry {
     /// current state of the connection
-    state: ChannelState,
+    state: std::sync::Mutex<ChannelState>,
 }
 
-impl ChannelMapEntry {}
+impl ChannelMapEntry {
+    fn handle_frame(&self, frame: Frame) -> Option<Frame> {
+        self.state.lock().unwrap().transition_recv(frame)
+    }
+}
 
 #[derive(Default)]
 enum ChannelState {
     #[default]
     Closed,
     SentOpen {
-        tx_on_established: oneshot::Sender<ChannelEstablishmentInfo>,
+        tx_on_established: oneshot::Sender<(ChannelTxActorWires, ChannelIo)>,
         our_rx_buffer: u64,
     },
     RecvdOpen {
         their_rx_buffer: u64,
     },
-    Established(ChannelEstablished),
+    Established(EstablishedChannelEntry),
     SentClose {
         tx_on_closed: oneshot::Sender<()>,
     },
@@ -159,7 +207,7 @@ impl ChannelState {
     fn try_transition_send_syn(
         &mut self,
         our_rx_buffer: u64,
-    ) -> Result<(Frame, oneshot::Receiver<ChannelEstablishmentInfo>), ChannelInUse> {
+    ) -> Result<(Frame, oneshot::Receiver<(ChannelTxActorWires, ChannelIo)>), ChannelInUse> {
         let syn = Frame::Syn(our_rx_buffer as u64);
         let (tx_on_established, rx_on_established) = oneshot::channel();
         match self {
@@ -171,43 +219,22 @@ impl ChannelState {
                 Ok((syn, rx_on_established))
             }
             ChannelState::RecvdOpen { their_rx_buffer } => {
-                let (est, info) =
-                    ChannelEstablished::new(our_rx_buffer as u64, *their_rx_buffer as u64);
+                let (entry, actor, user) =
+                    setup_established_channel_wires(our_rx_buffer as u64, *their_rx_buffer as u64);
 
-                *self = ChannelState::Established(est);
+                *self = ChannelState::Established(entry);
 
                 let (tx_on_established, rx_on_established) = oneshot::channel();
-                tx_on_established.send(info).map_err(|_| ()).unwrap(); // impossible to fail
+                tx_on_established
+                    .send((actor, user))
+                    .map_err(|_| ())
+                    .unwrap(); // impossible to fail
 
                 Ok((syn, rx_on_established))
             }
             ChannelState::SentOpen { .. }
             | ChannelState::Established(_)
             | ChannelState::SentClose { .. } => Err(ChannelInUse),
-        }
-    }
-
-    /// Returns the next frame to send on this channel, if any.
-    fn poll_next_frame(&mut self, cx: &mut Context<'_>) -> Poll<Option<Frame>> {
-        match self {
-            ChannelState::Established(channel_established) => {
-                match channel_established.tx.poll_next_send(cx) {
-                    Poll::Ready(Some(v)) => Poll::Ready(Some(Frame::Data(v))),
-                    Poll::Ready(None) => {
-                        // sender has closed. we should close ourselves up
-                        let (tx_on_closed, _rx_on_closed) = oneshot::channel();
-                        *self = ChannelState::SentClose { tx_on_closed };
-                        Poll::Ready(Some(Frame::Fin))
-                    }
-                    Poll::Pending => Poll::Pending,
-                }
-            }
-
-            ChannelState::SentOpen { .. }
-            | ChannelState::RecvdOpen { .. }
-            | ChannelState::SentClose { .. } => Poll::Pending,
-
-            ChannelState::Closed => Poll::Ready(None),
         }
     }
 
@@ -245,8 +272,7 @@ impl ChannelState {
         match self {
             ChannelState::Established(channel_established) => {
                 channel_established
-                    .rx
-                    .tx_to_consumer
+                    .tx_to_user
                     .try_send(body)
                     .expect("Failed");
                 None // Don't send ADM yet -- allow the frames to bunch up before ADM
@@ -306,10 +332,10 @@ impl ChannelState {
     }
 
     /// Handle receiving an ADM
-    fn transition_recv_adm(&mut self, permits: u64) -> Option<Frame> {
+    fn transition_recv_adm(&mut self, max_seqno: u64) -> Option<Frame> {
         match self {
             ChannelState::Established(channel_established) => {
-                channel_established.tx.outstanding_permits += permits;
+                channel_established.tx_max_seqno.send(max_seqno).ok();
                 None
             }
             ChannelState::SentClose { .. } => None,
@@ -332,97 +358,46 @@ pub struct ChannelInUse;
 #[error("Channel already closed")]
 struct AlreadyClosed;
 
-pub struct ChannelEstablishmentInfo {
-    pub tx: Sender,
-    pub rx: Receiver,
+pub struct ChannelIo {
+    tx: Sender,
+    rx: Receiver,
 }
 
-struct ChannelEstablished {
-    tx: ChannelTxState,
-    rx: ChannelRxState,
+struct EstablishedChannelEntry {
+    tx_max_seqno: watch::Sender<u64>,
+    tx_to_user: mpsc::Sender<Bytes>,
 }
 
-impl ChannelEstablished {
-    fn new(
-        our_rx_buffer: u64,
-        their_rx_buffer: u64,
-    ) -> (ChannelEstablished, ChannelEstablishmentInfo) {
-        let (tx_to_channel_map, rx_from_consumer) = mpsc::channel(their_rx_buffer as usize);
-        let (tx_to_consumer, rx_from_channel_map) = mpsc::channel(our_rx_buffer as usize);
-        let sender = Sender { tx_to_channel_map };
-        let receiver = Receiver {
-            rx_from_channel_map,
-        };
-        (
-            ChannelEstablished {
-                tx: ChannelTxState {
-                    rx_from_consumer,
-                    next_seq_no: 0,
-                    max_seq_no: their_rx_buffer as u64,
-                },
-                rx: ChannelRxState {
-                    tx_to_consumer,
-                    next_seq_no: 0,
-                    max_seq_no: our_rx_buffer as u64,
-                },
-            },
-            ChannelEstablishmentInfo {
-                tx: sender,
-                rx: receiver,
-            },
-        )
-    }
+struct ChannelTxActorWires {
+    rx_max_seqno: watch::Receiver<u64>,
+    rx_from_user: mpsc::Receiver<Bytes>,
 }
 
-struct ChannelTxState {
-    /// rx for receiving payloads from the consumer
-    rx_from_consumer: mpsc::Receiver<Bytes>,
+fn setup_established_channel_wires(
+    our_rx_buffer: u64,
+    their_rx_buffer: u64,
+) -> (EstablishedChannelEntry, ChannelTxActorWires, ChannelIo) {
+    let (tx_to_actor, rx_from_user) = mpsc::channel(their_rx_buffer as usize);
+    let (tx_to_user, rx_from_channel_map) = mpsc::channel(our_rx_buffer as usize);
+    let (tx_max_seqno, rx_max_seqno) = watch::channel(their_rx_buffer);
 
-    /// sequence number of the next frame we'll send
-    next_seq_no: u64,
+    let sender = Sender { tx_to_actor };
+    let receiver = Receiver {
+        rx_from_channel_map,
+    };
 
-    /// maximum sequence number that we're allowed to send
-    max_seq_no: u64,
-}
-
-impl ChannelTxState {
-    /// Attempt to pull a frame out for sending.
-    ///
-    /// Returns [Poll::Pending] if we are not ready to send, and [None] if we are out of
-    /// frames to send.
-    fn poll_next_send(&mut self, cx: &mut Context<'_>) -> Poll<Option<Bytes>> {
-        if self.next_seq_no > self.max_seq_no {
-            return Poll::Pending;
-        }
-
-        let out = self.rx_from_consumer.poll_recv(cx);
-        if let Poll::Ready(Some(v)) = &out {
-            self.next_seq_no += 1;
-        }
-        out
-    }
-}
-
-struct ChannelRxState {
-    /// tx for sending payloads to the consumer
-    tx_to_consumer: mpsc::Sender<Bytes>,
-
-    /// sequence number of the next frame we'll receive
-    next_seq_no: u64,
-
-    /// maximum sequence number that we're allowed to send
-    max_seq_no: u64,
-}
-
-impl ChannelRxState {
-    fn next_adm(&mut self) -> Option<Frame> {
-        let buf_available = self.tx_to_consumer.capacity() as u64;
-        let max_seq_no = self.next_seq_no + buf_available;
-        if self.max_seq_no > max_seq_no {
-            self.max_seq_no = max_seq_no;
-            Some(Frame::Adm(max_seq_no))
-        } else {
-            None
-        }
-    }
+    (
+        EstablishedChannelEntry {
+            tx_max_seqno,
+            tx_to_user,
+        },
+        ChannelTxActorWires {
+            rx_max_seqno,
+            rx_from_user,
+        },
+        ChannelIo {
+            tx: sender,
+            rx: receiver,
+        },
+    )
 }
