7da8684873dc45e902b8bb448a4fb2865af235cc
Astrid Yu <astrid@astrid.tech>
2026-05-03 20:48:10 -0700

    simplify types a bit

--- lib/stdiomux/src/codec/util.rs
+++ lib/stdiomux/src/codec/util.rs
@@ -4,30 +4,114 @@ use std::{
     task::{Context, Poll},
 };
 
-use futures::StreamExt;
-use pin_project::pin_project;
+use futures::{
+    StreamExt,
+    stream::{BoxStream, unfold},
+};
+
+use crate::mux::BoxByteStream;
 
 use super::*;
 
-/// Trivial identity codec for byte streams.
+/// Merges an [`Encoder`] and [`Decoder`] together into one [`Codec`].
+pub struct DelegateCodec<T, E, D, S> {
+    e: E,
+    d: D,
+    _phantom: PhantomData<fn(S) -> T>,
+}
+
+impl<T, E, D, S> DelegateCodec<T, E, D, S>
+where
+    E: Encoder<T>,
+    D: Decoder<T, S>,
+    S: Stream<Item = Bytes> + Send + Unpin + 'static,
+{
+    pub fn new(e: E, d: D) -> Self {
+        Self {
+            e,
+            d,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T, E, D, S> Clone for DelegateCodec<T, E, D, S>
+where
+    E: Encoder<T>,
+    D: Decoder<T, S>,
+    S: Stream<Item = Bytes> + Send + Unpin + 'static,
+{
+    fn clone(&self) -> Self {
+        Self {
+            e: self.e.clone(),
+            d: self.d.clone(),
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T, E, D, S> Encoder<T> for DelegateCodec<T, E, D, S>
+where
+    T: 'static,
+    E: Encoder<T>,
+    D: Decoder<T, S>,
+    S: Stream<Item = Bytes> + Send + Unpin + 'static,
+{
+    type SerializeStream = E::SerializeStream;
+
+    type SerializeError = E::SerializeError;
+
+    fn serialize(
+        &self,
+        value: T,
+        max_payload: usize,
+    ) -> Result<Self::SerializeStream, Self::SerializeError> {
+        self.e.serialize(value, max_payload)
+    }
+}
+
+impl<T, E, D, S> Decoder<T, S> for DelegateCodec<T, E, D, S>
+where
+    T: 'static,
+    E: Encoder<T>,
+    D: Decoder<T, S>,
+    S: Stream<Item = Bytes> + Send + Unpin + 'static,
+{
+    type DeserializeFuture = D::DeserializeFuture;
+
+    type DeserializeError = D::DeserializeError;
+
+    fn deserialize(&self, bss: S) -> Self::DeserializeFuture {
+        self.d.deserialize(bss)
+    }
+}
+
+/// Trivial identity encoder for byte streams.
 ///
 /// WARNING: Does not do any fragmentation if the bytes are bigger than the max payload!
 /// Do NOT use if that is a worry!
 #[derive(Clone)]
-pub struct ByteStreamCodec;
+pub struct ByteStreamEncoder;
+
+/// Trivial identity decoder for byte streams.
+#[derive(Clone)]
+pub struct ByteStreamDecoder;
 
 impl<S> Streamable<S> for S
 where
     S: Stream<Item = Bytes> + Send + Unpin + 'static,
 {
-    type Codec = ByteStreamCodec;
+    type Codec = DelegateCodec<S, ByteStreamEncoder, ByteStreamDecoder, S>;
 
     fn codec() -> Self::Codec {
-        ByteStreamCodec
+        DelegateCodec::<S, ByteStreamEncoder, ByteStreamDecoder, S>::new(
+            ByteStreamEncoder,
+            ByteStreamDecoder,
+        )
     }
 }
 
-impl<S> Encoder<S> for ByteStreamCodec
+impl<S> Encoder<S> for ByteStreamEncoder
 where
     S: Stream<Item = Bytes> + Send + Unpin + 'static,
 {
@@ -44,7 +128,7 @@ where
     }
 }
 
-impl<S> Decoder<S, S> for ByteStreamCodec
+impl<S> Decoder<S, S> for ByteStreamDecoder
 where
     S: Stream<Item = Bytes> + Send + Unpin + 'static,
 {
@@ -103,27 +187,28 @@ where
         value: S,
         max_payload: usize,
     ) -> Result<Self::SerializeStream, Self::SerializeError> {
+        let encoder = self.inner.clone();
         Ok(MapStreamEncoderSerializeStream {
-            encoder: self.inner.clone(),
-            input: value,
-            current: None,
-            max_payload,
+            inner: Box::pin(
+                value
+                    .filter_map(move |x| {
+                        let encoder = encoder.clone();
+                        async move { encoder.serialize(x, max_payload).ok() }
+                    })
+                    .flatten(),
+            ),
             _phantom: PhantomData,
         })
     }
 }
 
-#[pin_project]
 pub struct MapStreamEncoderSerializeStream<E, T, S>
 where
     E: Encoder<T>,
+    S: Stream<Item = T> + Send + Unpin + 'static,
 {
-    encoder: E,
-    input: S,
-    #[pin]
-    current: Option<Pin<Box<E::SerializeStream>>>,
-    max_payload: usize,
-    _phantom: PhantomData<fn() -> T>,
+    inner: BoxByteStream,
+    _phantom: PhantomData<fn(E, S) -> T>,
 }
 
 impl<E, T, S> Stream for MapStreamEncoderSerializeStream<E, T, S>
@@ -135,31 +220,9 @@ where
     type Item = Bytes;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        loop {
-            match self.current.as_mut() {
-                Some(c) => match c.poll_next_unpin(cx) {
-                    Poll::Ready(Some(bs)) => return Poll::Ready(Some(bs)),
-                    Poll::Ready(None) => {
-                        self.current = None;
-                    }
-                    Poll::Pending => return Poll::Pending,
-                },
-                None => match self.input.poll_next_unpin(cx) {
-                    Poll::Ready(Some(new)) => match self.encoder.serialize(new, self.max_payload) {
-                        Ok(something) => {
-                            self.current = Some(Box::pin(something));
-                        }
-                        Err(_er) => return Poll::Ready(None),
-                    },
-                    Poll::Ready(None) => return Poll::Ready(None),
-                    Poll::Pending => return Poll::Pending,
-                },
-            }
-        }
+        self.inner.poll_next_unpin(cx)
     }
 }
-
-/*
 
 /// Decoder that maps `Stream<Bytes>` into `Stream<T>`.
 pub struct MapStreamDecoder<D, T, S> {
@@ -169,7 +232,7 @@ pub struct MapStreamDecoder<D, T, S> {
 
 impl<D, T, S> From<D> for MapStreamDecoder<D, T, S>
 where
-    D: Decoder<T, MapStreamDecoderPseudoStream>,
+    D: Decoder<T, S>,
     T: Send + 'static,
     S: Stream<Item = Bytes> + Send + Unpin + 'static,
 {
@@ -187,14 +250,14 @@ where
     fn clone(&self) -> Self {
         Self {
             inner: self.inner.clone(),
-            _phantom: self._phantom.clone(),
+            _phantom: PhantomData,
         }
     }
 }
 
 impl<D, T, S> Decoder<MapStreamDecoderSerializeStream<D, T, S>, S> for MapStreamDecoder<D, T, S>
 where
-    D: Decoder<T, MapStreamDecoderPseudoStream>,
+    D: Decoder<T, S>,
     T: Send + 'static,
     S: Stream<Item = Bytes> + Send + Unpin + 'static,
 {
@@ -204,82 +267,46 @@ where
     type DeserializeError = Infallible;
 
     fn deserialize(&self, bss: S) -> Self::DeserializeFuture {
-        let cell = Arc::new(Mutex::new(None));
+        let decoder = self.inner.clone();
+        let fut = unfold(bss, move |mut bss| {
+            let decoder = decoder.clone();
+            async move {
+                let substream = unfold(&mut bss, |bss| async move {
+                    bss.next().await.map(|bs| (bs, bss))
+                });
+                let Ok(x) = decoder.deserialize(Box::pin(substream)).await else {
+                    return None;
+                };
+
+                Some((x, bss))
+            }
+        });
+
         future::ready(Ok(MapStreamDecoderSerializeStream {
-            decoder: self.inner.clone(),
-            input: bss,
-            pseudostream_cell: cell,
-            current: None,
+            inner: Box::pin(fut),
             _phantom: PhantomData,
         }))
     }
 }
 
-#[pin_project]
 pub struct MapStreamDecoderSerializeStream<D, T, S>
 where
-    D: Decoder<T, MapStreamDecoderPseudoStream>,
+    D: Decoder<T, S>,
     S: Stream<Item = Bytes> + Send + Unpin + 'static,
 {
-    decoder: D,
-    #[pin]
-    input: S,
-    pseudostream_cell: Arc<Mutex<Option<Bytes>>>,
-    current: Option<Pin<Box<D::DeserializeFuture>>>,
-    _phantom: PhantomData<fn() -> T>,
-}
-
-#[pin_project]
-pub struct MapStreamDecoderPseudoStream {
-    #[pin]
-    cell: Arc<Mutex<Option<Bytes>>>,
-    wake: AtomicWaker,
-}
-
-impl Stream for MapStreamDecoderPseudoStream {
-    type Item = Bytes;
-
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        match self.cell.lock().unwrap().take() {
-            Some(x) => Poll::Ready(Some(x)),
-            None => {
-                self.wake.register(cx.waker());
-                Poll::Pending
-            }
-        }
-    }
+    inner: BoxStream<'static, T>,
+    _phantom: PhantomData<fn(D, S) -> T>,
 }
 
 impl<D, T, S> Stream for MapStreamDecoderSerializeStream<D, T, S>
 where
-    D: Decoder<T, MapStreamDecoderPseudoStream>,
+    D: Decoder<T, S>,
     T: Send + 'static,
     S: Stream<Item = Bytes> + Send + Unpin + 'static,
 {
-    type Item = Bytes;
+    type Item = T;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        loop {
-            match self.current {
-                Some(x) => match self.input.poll_next_unpin(cx) {
-                    Poll::Ready(r) => todo!(),
-                    Poll::Ready(None) => match x.poll_unpin(cx) {
-                        Poll::Ready(r) => r.ok().,
-                        Poll::Pending => todo!(),
-                    },
-                    Poll::Pending => todo!(),
-                },
-                None => {
-                    todo!()
-                    self.current = Some(Box::pin(self.decoder.deserialize(
-                        MapStreamDecoderPseudoStream {
-                            cell: self.pseudostream_cell.clone(),
-                            wake: AtomicWaker::new(),
-                        },
-                    )));
-                }
-            }
-        }
+        self.inner.poll_next_unpin(cx)
     }
 }
-*/
