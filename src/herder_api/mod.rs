2cb25166e42be36776b1a974b151041aef4716ea
Astrid Yu <astrid@astrid.tech>
2026-05-04 23:32:26 -0700

    make a service trait (wip)

--- src/herder_api/mod.rs
+++ src/herder_api/mod.rs
@@ -1,74 +1,123 @@
 pub mod write_verify;
 
-use std::fmt::{Debug, Display};
-
+use futures::{Stream, future::BoxFuture, stream::BoxStream};
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
+use std::{
+    error::Error,
+    fmt::Debug,
+    marker::PhantomData,
+    sync::Arc,
+    task::{Context, Poll},
+};
+use stdiomux::codec::{
+    Streamable,
+    postcard::{SingleDatagramCodec, SingleDatagramCodecDeserializeFuture},
+};
+use tower::Service;
 
-/// Tell the herder to start a herd for performing an arbitrary action.
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct StartHerd<A> {
-    /// ID to associate with all of the herd's events
-    pub id: u64,
+use crate::herder_api::write_verify::WriteVerifyAction;
 
-    /// The action to perform
-    pub action: A,
-}
+/// Maximum payload size that can be sent on the wire.
+pub const MAX_PAYLOAD: usize = 4096;
 
 /// Arbitrary herd initialization action. This can be anything, from writing to verifying to voiding.
-pub trait HerdAction:
-    Serialize + DeserializeOwned + Debug + Clone + PartialEq + Send + 'static
-{
-    /// The events emitted by the herd afterwards.
-    type Event: HerdEvent;
+pub trait HerderAction: Message + Into<TopLevelHerderAction> {
+    /// Initial information from the herder.
+    type Start: Message;
+
+    /// Errors that the herd can emit.
+    type Error: Message + std::error::Error;
+
+    /// The events emitted by the herd after being started.
+    type Event: Message;
+
+    /// The final success message.
+    type Done: Message;
 }
 
-/// An event emitted by a running herd.
-pub trait HerdEvent:
-    Serialize
-    + DeserializeOwned
-    + Debug
-    + Clone
-    + PartialEq
-    + TryFrom<TopLevelHerdEvent, Error = TopLevelHerdEvent>
-    + Send
-    + 'static
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub enum HerdEvent<A: HerderAction> {
+    Event(A::Event),
+    Done(A::Done),
+    Error(A::Error),
+}
+
+/// Trait alias for things that we support sending over a wire.
+pub trait Message:
+    Serialize + DeserializeOwned + Debug + Clone + PartialEq + Send + 'static
 {
-    /// The initial information variant that it's expected to send out as soon as it
-    /// has started running.
-    type StartInfo: Debug;
+}
 
-    /// A failure variant indicating that this herd has terminated unexpectedly and fatally
-    /// without any hope of recovery.
-    type Failure: Display + Debug;
-
-    /// Downcast this event trait into its InitialInfo variant.
-    fn downcast_as_initial_info(self) -> Result<Self::StartInfo, Self>;
-
-    /// Downcast this event trait into its failure variant.
-    fn downcast_as_failure(self) -> Result<Self::Failure, Self>;
+impl<T> Message for T where
+    T: Serialize + DeserializeOwned + Debug + Clone + PartialEq + Send + 'static
+{
 }
 
 /// An enum containing all implemented and valid types of herder event.
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, derive_more::From)]
+///
+/// This doesn't actually implement [`HerderAction`] because its responses are type-erased
+/// on the wire. It exists purely for serialization.
+#[derive(
+    Debug, Clone, PartialEq, Serialize, Deserialize, derive_more::From, derive_more::TryInto,
+)]
 #[non_exhaustive]
-pub enum TopLevelHerdEvent {
-    Writer(write_verify::WriteVerifyEvent),
+pub enum TopLevelHerderAction {
+    Writer(WriteVerifyAction),
 }
 
-macro_rules! impl_try_from_top_level_herd_event {
-    ($arm:ident => $event_type:ty) => {
-        impl TryFrom<crate::herder_api::TopLevelHerdEvent> for $event_type {
-            type Error = crate::herder_api::TopLevelHerdEvent;
-            fn try_from(
-                ev: crate::herder_api::TopLevelHerdEvent,
-            ) -> Result<Self, crate::herder_api::TopLevelHerdEvent> {
-                match ev {
-                    crate::herder_api::TopLevelHerdEvent::$arm(x) => Ok(x),
-                    //other => Err(other),
-                }
-            }
+pub struct HerdStarted<A: HerderAction> {
+    pub start: A::Start,
+    pub events: BoxStream<'static, HerdEvent<A>>,
+}
+
+/// Abstract interface to a herder daemon.
+pub trait HerderService {
+    type Error: std::error::Error;
+
+    fn start_action<A: HerderAction>(
+        &mut self,
+        a: A,
+    ) -> impl Future<Output = Result<HerdStarted<A, Self::Error>, Self::Error>> + Send + 'static;
+}
+
+/// Adapter from [`HerderService`] to [`tower::Service`]
+pub struct HerderTowerService<A, S> {
+    inner: S,
+    _phantom: PhantomData<fn(A)>,
+}
+
+impl<A, S> Clone for HerderTowerService<A, S> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl<A, S> From<S> for HerderTowerService<A, S> {
+    fn from(inner: S) -> Self {
+        HerderTowerService {
+            inner,
+            _phantom: PhantomData,
         }
-    };
+    }
 }
 
-pub(self) use impl_try_from_top_level_herd_event;
+impl<A, S> Service<A> for HerderTowerService<A, S>
+where
+    A: HerderAction,
+    S: HerderService,
+{
+    type Response = BoxStream<'static, Result<A::Event, A::Error>>;
+
+    type Error = S::Error;
+
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: A) -> Self::Future {
+        let fut = self.0.start_action(req);
+        Box::pin(fut)
+    }
+}
--- src/herder_api/write_verify.rs
+++ src/herder_api/write_verify.rs
@@ -1,8 +1,9 @@
-use super::HerdAction;
+use std::path::PathBuf;
+
+use serde::{Deserialize, Serialize};
+
 use crate::compression::CompressionFormat;
 use crate::device::Type;
-use serde::{Deserialize, Serialize};
-use std::{fmt::Display, path::PathBuf};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct WriteVerifyAction {
@@ -14,13 +15,18 @@ pub struct WriteVerifyAction {
     pub block_size: Option<u64>,
 }
 
-impl HerdAction for WriteVerifyAction {
+impl super::HerderAction for WriteVerifyAction {
+    type Start = WriteVerifyStart;
+
+    type Error = WriteVerifyError;
+
     type Event = WriteVerifyEvent;
+    
+    type Done = ();
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum WriteVerifyEvent {
-    InitSuccess(WriteVerifyStart),
     TotalBytes {
         src: u64,
         dest: u64,
@@ -34,29 +40,6 @@ pub enum WriteVerifyEvent {
         block_size: usize,
         duration_millis: u64,
     },
-    Success,
-    Error(WriteVerifyError),
-}
-
-super::impl_try_from_top_level_herd_event!(Writer => WriteVerifyEvent);
-
-impl super::HerdEvent for WriteVerifyEvent {
-    type StartInfo = WriteVerifyStart;
-    type Failure = WriteVerifyError;
-
-    fn downcast_as_initial_info(self) -> Result<Self::StartInfo, Self> {
-        match self {
-            WriteVerifyEvent::InitSuccess(e) => Ok(e),
-            other => Err(other),
-        }
-    }
-
-    fn downcast_as_failure(self) -> Result<Self::Failure, Self> {
-        match self {
-            WriteVerifyEvent::Error(e) => Ok(e),
-            other => Err(other),
-        }
-    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -64,15 +47,20 @@ pub struct WriteVerifyStart {
     pub input_file_bytes: u64,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, thiserror::Error)]
 pub enum WriteVerifyError {
+    #[error("Unexpected end of output file. Is your output file too small?")]
     EndOfOutput,
+    #[error("Permission denied while opening file")]
     PermissionDenied,
+    #[error("Disk verification failed!")]
     VerificationFailed,
+    #[error("The child process unexpectedly terminated!")]
     UnexpectedTermination,
+    #[error("Unknown error occurred in child process: {0}")]
     UnknownChildProcError(String),
+    #[error("Failed to unmount disk (exit code {exit_code})\n{message}")]
     FailedToUnmount { message: String, exit_code: i32 },
-    Panicked,
 }
 
 impl From<std::io::Error> for WriteVerifyError {
@@ -80,30 +68,6 @@ impl From<std::io::Error> for WriteVerifyError {
         match value.kind() {
             std::io::ErrorKind::PermissionDenied => Self::PermissionDenied,
             _ => Self::UnknownChildProcError(format!("{value:#}")),
-        }
-    }
-}
-
-impl Display for WriteVerifyError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            WriteVerifyError::EndOfOutput => write!(
-                f,
-                "Unexpected end of output file. Is your output file too small?"
-            ),
-            WriteVerifyError::PermissionDenied => write!(f, "Permission denied while opening file"),
-            WriteVerifyError::VerificationFailed => write!(f, "Disk verification failed!"),
-            WriteVerifyError::UnexpectedTermination => {
-                write!(f, "The child process unexpectedly terminated!")
-            }
-            WriteVerifyError::UnknownChildProcError(err) => {
-                write!(f, "Unknown error occurred in child process: {err}")
-            }
-            WriteVerifyError::FailedToUnmount { message, exit_code } => write!(
-                f,
-                "Failed to unmount disk (exit code {exit_code})\n{message}"
-            ),
-            WriteVerifyError::Panicked => write!(f, "Orchestrator panicked!"),
         }
     }
 }
