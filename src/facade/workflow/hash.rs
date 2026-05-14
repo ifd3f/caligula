31413421deec5c9c0f9d245a031652f172054e55
Astrid Yu <astrid@astrid.tech>
2026-05-13 17:49:05 -0700

    Revert "try (but fail) to make faster mmap based thing"
    This reverts commit 751f5341e63e2048087b747f275035408c0eb879.

--- src/facade/workflow/hash.rs
+++ src/facade/workflow/hash.rs
@@ -1,7 +1,6 @@
 use std::{path::PathBuf, sync::Arc, time::Instant};
 
 use bytes::Bytes;
-use bytesize::ByteSize;
 use tokio::{
     sync::{oneshot, watch},
     task::JoinHandle,
@@ -127,11 +126,11 @@ fn run_thread(
 ) {
     std::thread::scope(move |s| {
         let setup = (|| {
-            let buf = BufNode::new((ByteSize::mb(64).as_u64() / 4096).try_into().unwrap());
+            let buf = BufNode::new(1024);
 
             let j = js.create();
 
-            let read = FileReader::new(&wf.file, buf.input)?;
+            let read = FileReader::new(&wf.file, buf.input, 65536)?;
             let start_data = StartData {
                 size: read.size(),
                 hasher_input_junction: j.id(),
