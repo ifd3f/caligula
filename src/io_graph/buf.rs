d560b1aeb482ed54d36acf8cb96d4da6e1ba7969
Astrid Yu <astrid@astrid.tech>
2026-05-13 23:24:59 -0700

    Revert "try triple buffering? doesn't really help"
    This reverts commit 1db2403679370b56bfacd5125a39dd3fe18236c4.

--- src/io_graph/buf.rs
+++ src/io_graph/buf.rs
@@ -1,5 +1,4 @@
 use std::{
-    cell::UnsafeCell,
     marker::PhantomData,
     sync::{
         Arc, Mutex, OnceLock,
@@ -9,7 +8,6 @@ use std::{
     time::Duration,
 };
 
-use atomicbox::AtomicBox;
 use bytes::Bytes;
 use ringbuf::{
     LocalRb,
@@ -19,19 +17,18 @@ use ringbuf::{
 
 use crate::io_graph::{RecvBytes, SendBytes};
 
-const PARK_TIMEOUT: Duration = Duration::from_millis(2);
-type Rb = LocalRb<Heap<Bytes>>;
+const PARK_TIMEOUT: Duration = Duration::from_millis(1);
 
 /// Create a new paired [`BufSender`] and [`BufReceiver`].
 ///
-/// This is backed by triple buffering. with initial capacity of all three
-/// buffers set to `size`.
+/// This is backed by double buffering, with initial capacity set to
+/// `size`.
 ///
 /// These cannot be used until they are each assigned onto a thread by calling
 /// `.into_local()`.
 pub fn buf(size: usize) -> (BufSender, BufReceiver) {
     let inner = Arc::new(Inner {
-        middle_buffer: MiddleBuffer::new(size),
+        back_buffer: Mutex::new(LocalRb::new(size)),
         receiver: OnceLock::new(),
         sender: OnceLock::new(),
         receiver_dropped: false.into(),
@@ -60,10 +57,8 @@ impl BufSender {
             .set(std::thread::current())
             .expect("Inner's sender thread already set!");
 
-        let size = self.inner.size;
         LocalBufSender {
             inner: self.inner,
-            back_buffer: Box::new(LocalRb::new(size)),
             _phantom: PhantomData,
         }
     }
@@ -84,13 +79,14 @@ impl BufReceiver {
         let size = self.inner.size;
         LocalBufReceiver {
             inner: self.inner,
-            front_buf: Box::new(LocalRb::new(size)),
+            front_buf: LocalRb::new(size),
             _phantom: PhantomData,
         }
     }
 }
 struct Inner {
-    middle_buffer: MiddleBuffer,
+    /// The back buffer that gets appended to.
+    back_buffer: Mutex<LocalRb<Heap<Bytes>>>,
 
     /// Flag for the current state of the buffer.
     eof: AtomicBool,
@@ -111,52 +107,6 @@ struct Inner {
     size: usize,
 }
 
-/// The middle buffer is the one point of shared contention between the sender
-/// and receiver.
-struct MiddleBuffer {
-    buf: AtomicBox<Rb>,
-    is_empty: AtomicBool,
-}
-
-impl MiddleBuffer {
-    fn new(size: usize) -> Self {
-        Self {
-            buf: AtomicBox::new(Box::new(LocalRb::new(size))),
-            is_empty: true.into(),
-        }
-    }
-
-    /// Attempt to swap in a buffer that may or may not contain data.
-    ///
-    /// Returns whether or not the swap went through.
-    fn try_swap(&self, replacement: &mut Box<Rb>) -> bool {
-        match (
-            replacement.is_empty(),
-            self.is_empty.load(Ordering::Relaxed),
-        ) {
-            // Both empty -- don't swap, it's a no-op
-            (true, true) => false,
-
-            // Both full -- don't swap, data will be overwritten
-            (false, false) => false,
-
-            // Replacement empty, self full: this is a take operation, mark us as empty
-            (true, false) => {
-                self.buf.swap_mut(replacement, Ordering::SeqCst);
-                self.is_empty.store(true, Ordering::SeqCst);
-                true
-            }
-
-            // Replacement full, self empty: this is a put operation, mark us as full
-            (false, true) => {
-                self.buf.swap_mut(replacement, Ordering::SeqCst);
-                self.is_empty.store(false, Ordering::SeqCst);
-                true
-            }
-        }
-    }
-}
-
 impl Inner {
     fn drop_sender(&self) {
         self.sender_dropped.store(true, Ordering::Relaxed);
@@ -172,9 +122,6 @@ impl Inner {
 #[must_use]
 pub struct LocalBufSender {
     inner: Arc<Inner>,
-
-    /// Active buffer we're writing to.
-    back_buffer: Box<Rb>,
 
     /// Used to force this to be non-[`Send`].
     _phantom: PhantomData<*const ()>,
@@ -197,38 +144,31 @@ impl SendBytes for LocalBufSender {
             }
 
             // Try to insert into the back buffer.
-            match self.back_buffer.try_push(bytes) {
+            //
+            // This operation is unlikely to have contention because the receiver holds this
+            // lock for an extremely short period of time.
+            let mut lock = self.inner.back_buffer.lock().unwrap();
+            let queue = lock.as_mut();
+
+            let result = queue.try_push(bytes);
+            drop(lock);
+
+            match result {
                 Ok(()) => {
-                    // Successfully inserted, we're done here
+                    // Successfully inserted: wake up the receiver
+                    self.inner.receiver.wait().unpark();
                     return Ok(());
                 }
                 Err(not_appended) => {
-                    // It's full: we'll have to go empty it by swapping with middle buffer.
+                    // It's full: park until we get woken up, then try again
                     bytes = not_appended;
-                }
-            }
-
-            match self.inner.middle_buffer.try_swap(&mut self.back_buffer) {
-                true => {
-                    // Successfully went through. Wake up the receiver, loop around, and insert into
-                    // the freshly empty buffer.
-                    self.inner.receiver.wait().unpark();
-                }
-                false => {
-                    // Didn't work. Park, loop, and try again
                     park_timeout(PARK_TIMEOUT);
                 }
             }
         }
     }
 
-    fn close(mut self) -> std::io::Result<()> {
-        // put the rest of the buffered data into the middle buffer
-        while !self.back_buffer.is_empty() {
-            self.inner.middle_buffer.try_swap(&mut self.back_buffer);
-            park_timeout(PARK_TIMEOUT);
-        }
-
+    fn close(self) -> std::io::Result<()> {
         // Set the EOF flag and wake the reader
         self.inner.eof.store(true, Ordering::SeqCst);
         self.inner.receiver.wait().unpark();
@@ -247,7 +187,7 @@ pub struct LocalBufReceiver {
     inner: Arc<Inner>,
 
     /// Current buffer we're reading from.
-    front_buf: Box<Rb>,
+    front_buf: LocalRb<Heap<Bytes>>,
 
     /// Used to force this to be non-[`Send`].
     _phantom: PhantomData<*const ()>,
@@ -256,33 +196,33 @@ pub struct LocalBufReceiver {
 impl RecvBytes for LocalBufReceiver {
     fn recv(&mut self) -> std::io::Result<Option<Bytes>> {
         loop {
-            // Try to pull from the back buffer.
-            if let Some(out) = self.front_buf.try_pop() {
-                return Ok(Some(out));
+            if let Some(val) = self.front_buf.try_pop() {
+                // item is available, so return it
+                return Ok(Some(val));
             }
 
-            // It's empty: we'll have to go swap with middle buffer.
-            if self.inner.middle_buffer.try_swap(&mut self.front_buf) {
-                // Successfully went through. Wake up the sender, loop around, and pull from
-                // the freshly filled buffer.
-                self.inner.sender.wait().unpark();
+            // front buffer is empty, try taking the back buffer
+            let mut lock = self.inner.back_buffer.lock().unwrap();
+            std::mem::swap(lock.as_mut(), &mut self.front_buf);
+            drop(lock);
+
+            if !self.front_buf.is_empty() {
+                // we have new elements -- loop around again
+                continue;
             }
 
-            // No new data. Ensure not EOF
-            if self.inner.eof.load(Ordering::Relaxed) {
+            // no new elements. are we EOF?
+            if self.inner.eof.load(Ordering::SeqCst) {
                 return Ok(None);
             }
 
-            // Not EOF. Ensure receiver hasn't dipped
+            // no new elements, and not EOF. did sender dip?
             if self.inner.sender_dropped.load(Ordering::Relaxed) {
                 return Err(std::io::Error::new(
                     std::io::ErrorKind::BrokenPipe,
-                    "buf receiver dropped",
+                    "buf sender dropped",
                 ));
             }
-
-            // Park, loop, and try again
-            park_timeout(PARK_TIMEOUT);
         }
     }
 }
