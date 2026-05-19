ffc2c2bc0f17dcb7f1041091f1c890993d4a68d2
Astrid Yu <astrid@astrid.tech>
2026-05-19 12:01:35 -0700

    Revert "stddisk"
    This reverts commit b6910f4e5c6699cc11752a452caa5d717fdd5bde.

--- src/io_graph/worker/mod.rs
+++ src/io_graph/worker/mod.rs
@@ -2,12 +2,10 @@
 
 pub use self::{
     decompress::{DecompressError, DecompressorWorker},
-    disk_writer::std::StdDiskWriter,
     file_reader::FileReader,
     hash::HashWorker,
 };
 
 mod decompress;
-mod disk_writer;
 mod file_reader;
 mod hash;
