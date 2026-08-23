dbd6e5d31b75ef32031e1de479bddfa45fd8da63
Astrid Yu <astrid@astrid.tech>
2026-08-23 11:53:47 -0700

    delete stdiomux

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -6,11 +6,11 @@
 //! - Self-contained functions and utilities that are fairly small, but shared
 //!   enough that they don't really make much sense elsewhere
 //! - Libraries that would theoretically make sense to split into their own
-//!   crates later, like [stdiomux] and [io_graph]
+//!   crates later, like [io_graph]
 //!
 //! As a rule of thumb, if a utility is only used by one subsystem, it should go
 //! inside there rather than here. The exception to this is rule those
-//! aforementioned large libraries like [stdiomux] and [io_graph].
+//! aforementioned large libraries like [io_graph].
 
 use std::{
     alloc::{Layout, alloc},
@@ -30,7 +30,6 @@ pub mod device;
 pub mod io_graph;
 pub mod legacy_io;
 pub mod runtime;
-pub mod stdiomux;
 
 /// Create the directory to shove invocation-specific data into, like log files
 /// and sockets.
